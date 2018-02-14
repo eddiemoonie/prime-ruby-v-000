b856917cde56adaef83bd8046c580239c8b7e62d
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 22:29:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 def prime?(number)
   if number < 0 || number == 0 || number == 1
     return false
+  binding.pry
   else
     (2..number-1).to_a.all? do |i|
       number % i != 0
