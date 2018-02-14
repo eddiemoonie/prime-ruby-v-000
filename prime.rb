6099909df33627cc066e3fab0e28d61229189216
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 22:24:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(number)
   if number < 0 || number == 0 || number == 1
     return false
   else
-    (2..number-1).to_a.all? do |divisible|
-      number % divisible != 0
+    (2..number-1).to_a.all? do |i|
+      number % i != 0
     end
   end
 end
