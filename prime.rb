57d59f7bed1c7cc9344d3331fd47cd88e52d9b71
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 22:11:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number < 0 || number == 0 || number == 1
     return false
   else
-    (2..number).to_a.all? do |divisible|
+    (2..number-1).to_a.all? do |divisible|
       number % divisible != 0
     end
   end
